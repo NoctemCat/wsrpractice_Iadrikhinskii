5fb88078b2488f2c64b16659c05a4f5c37750a88
NoctemCat <ya7kolya@gmail.com>
2021-03-05 00:39:00 +0900

    More changes

--- app/src/main/java/com/example/wsr_iadrikhinskii/MainActivity.java
+++ app/src/main/java/com/example/wsr_iadrikhinskii/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.wsr_iadrikhinskii;
 
+import androidx.annotation.ColorRes;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -27,12 +44,13 @@ public class MainActivity extends AppCompatActivity {
         final TextView usd_textView = (TextView)findViewById(R.id.text_curusd);
         final TextView eur_textView = (TextView)findViewById(R.id.text_cureur);
 
+
         Date todayDate = Calendar.getInstance().getTime();
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         String todayString = formatter.format(todayDate);
 
         CurrencyAPI api = CurrencyAPI.retrofit.create(CurrencyAPI.class);
-
+/*
         final Call<ValCurs> call = api.getValutes(todayString);
 
         call.enqueue(new Callback<ValCurs>() {
@@ -69,6 +87,67 @@ public class MainActivity extends AppCompatActivity {
                 date_textView.setText("Что-то пошло не так: " + t.getMessage());
             }
         });
+*/
+    }
 
+    public void click_ATM(View view){
+        Intent intent = new Intent(this, bankomats.class);
+        startActivity(intent);
+    }
+
+    public void click_Currency(View view){
+
+    }
+
+    public void click_autorization(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_NoActionBar_MinWidth);
+        builder.setTitle("Авторизация");
+        builder.setCancelable(false);
+
+        RelativeLayout layout = new RelativeLayout(this);
+        layout.setGravity(Gravity.CENTER);
+        LinearLayout lin = new LinearLayout(this);
+        lin.setOrientation(LinearLayout.VERTICAL);
+        lin.setGravity(Gravity.CENTER);
+
+        TextView v = new TextView(this);
+        v.setText("Введите ваш логин или пароль");
+        v.setGravity(Gravity.CENTER_HORIZONTAL);
+        v.setTextSize(18);
+        v.setTextColor(getColor(R.color.grey));
+        lin.addView(v);
+
+        LinearLayout.LayoutParams lpView = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        EditText login = new EditText(this);
+        login.setLayoutParams(lpView);
+        login.setText("");
+        login.setTextColor(getColor(R.color.white));
+        login.setHint("Логин");
+        login.setHintTextColor(getColor(R.color.grey));
+        lin.addView(login);
+
+        EditText pass = new EditText(this);
+        pass.setLayoutParams(lpView);
+        pass.setText("");
+        pass.setTextColor(getColor(R.color.white));
+        pass.setHint("Пароль");
+        pass.setHintTextColor(getColor(R.color.grey));
+        pass.setInputType(129);
+        lin.addView(pass);
+
+        layout.addView(lin);
+        builder.setView(layout);
+
+        builder.setNegativeButton("Отмена", (dialog, which) -> {
+            Log.v("cross", "fail " + login.getText() + " " + pass.getText());
+        });
+        builder.setPositiveButton("Войти", (dialog, which) -> {
+            Log.v("cross", "pass " + login.getText() + " " + pass.getText());
+        });
+
+        //builder.setView(but.getView());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
